1cab8ce749ed1b234b723e0e8bbfb1ffac469a0d
darinadari13 <darina460@gmail.com>
2022-07-07 12:38:03 +0300

    commit

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import './WeatherForecast.css';
 import axios from 'axios';
 import WeatherForecastDay from "./WeatherForecastDay";
@@ -7,6 +7,10 @@ import WeatherForecastDay from "./WeatherForecastDay";
 export default function WeatherForecast(props) {
     let [loaded, setLoaded] = useState(false);
     let [forecast, setForecast] = useState(null);
+
+    useEffect(() => {
+      setLoaded(false);  
+    }, [props.coordinates])
 
     function handleResponse(response){
         setForecast(response.data.daily);
@@ -22,6 +26,8 @@ export default function WeatherForecast(props) {
                     return  <div className='col' key={index}>
                     <WeatherForecastDay data={dailyForecast}/>
                 </div>
+                }else {
+                    return null;
                 }
             }
         )}
